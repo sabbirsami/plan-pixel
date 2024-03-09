d82a58cae1b4c1676470787419a057a4933f67d6
Sabbir Mohammad Sami <smd71430@gmail.com>
2024-03-09 13:36:29 +0600

    Update useAxios.js

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const axiosInstance = axios.create({
     // baseURL: "http://localhost:5000",
     // baseURL: "https://plan-pixel-backend-jet.vercel.app",
-    // baseURL: "https://plan-pixel-backend.vercel.app",
-    baseURL: "https://plan-pixel-backend-xi.vercel.app",
+    baseURL: "https://plan-pixel-backend.vercel.app",
+   // baseURL: "https://plan-pixel-backend-xi.vercel.app",
 });
 
 const useAxios = () => {
